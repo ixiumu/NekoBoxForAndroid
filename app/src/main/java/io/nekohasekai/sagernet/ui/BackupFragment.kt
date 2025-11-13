34a749a2f27091aa166eabf04759ca7293707b41
ixiumu <ixiumu@users.noreply.github.com>
2025-11-13 18:00:26 +0800

    Remove Ads

--- app/src/main/java/io/nekohasekai/sagernet/ui/BackupFragment.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ui/BackupFragment.kt
@@ -84,7 +84,7 @@ class BackupFragment : NamedFragment(R.layout.layout_backup) {
                 )
                 onMainDispatcher {
                     startFilesForResult(
-                        exportSettings, "nekobox_backup_${Date().toLocaleString()}.json"
+                        exportSettings, "outline_backup_${Date().toLocaleString()}.json"
                     )
                 }
             }
@@ -99,7 +99,7 @@ class BackupFragment : NamedFragment(R.layout.layout_backup) {
                 )
                 app.cacheDir.mkdirs()
                 val cacheFile = File(
-                    app.cacheDir, "nekobox_backup_${Date().toLocaleString()}.json"
+                    app.cacheDir, "outline_backup_${Date().toLocaleString()}.json"
                 )
                 cacheFile.writeText(content)
                 onMainDispatcher {
--- buildSrc/src/main/kotlin/Helpers.kt
+++ buildSrc/src/main/kotlin/Helpers.kt
@@ -197,10 +197,10 @@ fun Project.setupApp() {
                 outputFileName = if (isPreview) {
                     outputFileName.replace(
                         project.name,
-                        "NekoBox-" + requireMetadata().getProperty("PRE_VERSION_NAME")
+                        "Outline-" + requireMetadata().getProperty("PRE_VERSION_NAME")
                     ).replace("-preview", "")
                 } else {
-                    outputFileName.replace(project.name, "NekoBox-$versionName")
+                    outputFileName.replace(project.name, "Outline-$versionName")
                         .replace("-release", "")
                         .replace("-oss", "")
                 }
